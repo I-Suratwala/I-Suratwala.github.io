e9b8994abda9166bd0c5c953b3e6bf1b39bf08be
I-Suratwala <66978764+I-Suratwala@users.noreply.github.com>
2020-06-20 16:53:46 -0400

    V 0.0.5

--- game.js
+++ game.js
@@ -103,7 +103,6 @@ function create ()
 
     //  The score
     scoreText = this.add.text(16, 16, 'score: 0', { fontWeight: "bold", fontSize: '32px', fill: '#000' });
-    var highScoreText = this.add.text(16,35, "highscore: 0", {fontWeight: "bold", fontSize: "32px", fill: "#000"});
     //  Collide the player and the stars with the platforms
     this.physics.add.collider(player, platforms);
     this.physics.add.collider(stars, platforms);
